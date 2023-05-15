745ece284f6ef18dc45d61f60f6efdb644686ca7
codereptile <codereptile@yandex.ru>
2023-05-16 01:15:01 +0200

    remove unused files

--- image_warrior.cpp
+++ image_warrior.cpp
@@ -8,8 +8,6 @@
 #include <boost/filesystem.hpp>
 #include <opencv2/opencv.hpp>
 
-#include <file_utils.h>
-
 struct StderrSuppressor {
     StderrSuppressor() {
         fflush(stderr);
@@ -163,6 +161,31 @@ public:
     }
 
 private:
+    static cv::Mat LoadImage(const boost::filesystem::path &file_path) {
+        auto image = cv::imread(file_path.string());
+        if (image.empty()) {
+            throw std::runtime_error("Failed to load image: " + file_path.string());
+        }
+        return image;
+    }
+
+    static bool IsImageFile(const boost::filesystem::path &file_path) {
+        boost::filesystem::path extension = file_path.extension();
+        std::string ext_str = extension.string();
+        std::transform(ext_str.begin(), ext_str.end(), ext_str.begin(), ::tolower);
+
+        const std::vector<std::string> image_extensions = {
+                ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif",
+                ".ico", ".jfif", ".webp", ".svg", ".svgz", ".eps", ".pcx",
+                ".raw", ".cr2", ".nef", ".orf", ".sr2", ".heic", ".heif",
+                ".pdf", ".ai", ".indd", ".qxd", ".qxp", ".jp2", ".jpx",
+                ".j2k", ".j2c", ".wdp", ".hdp", ".exr", ".dng", ".cr3"
+        };
+
+        return std::find(image_extensions.begin(), image_extensions.end(), ext_str) != image_extensions.end();
+    }
+
+
     void ImageProcessingThread() {
         size_t processed_images_count = 0;
 
